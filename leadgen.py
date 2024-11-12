77c8535aa2aac78e0f47702a15fa0b57d688f0ef
rickert156 <rickert156@rambler.ru>
2024-11-12 16:29:57 +0400

    new commit

--- leadgen.py
+++ leadgen.py
@@ -2,7 +2,6 @@ import csv, os
 from tools.selectFile import selectFile
 from tools.readBase import readBaseApollo, checkEmail, EMAIL_LIST
 from tools.RecordData import BASE_FILE
-#from tools.Comparison import ComparisonDomain
            
 def startBase():
     t = "Email,Name,Job Title,Company,Domain,Location,Phone,Linkedin,Twitter,Facebook"
